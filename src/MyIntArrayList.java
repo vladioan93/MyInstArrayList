f95eb650dd12558bbbde45d1d3b37f849d889630
Vlad Ioan <prioteasa.vlad@gmail.com>
2020-03-08 14:00:42 +0200

    First commit

---
+++ src/MyIntArrayList.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+
+public class MyIntArrayList {
+
+    private int[] elements;
+    private int size;
+
+    public MyIntArrayList() {
+        this(10);
+    }
+
+    public MyIntArrayList(int initialCapacity) {
+        this.elements = new int[initialCapacity];
+    }
+
+    public boolean add(int e) {
+        if (this.size == this.elements.length) {
+            this.elements = Arrays.copyOf(this.elements, this.size * 2);
+        }
+
+        this.elements[size] = e;
+        this.size++;
+
+        return true;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int indexOf(int e) {
+        for (int i=0; i<this.size; i++) {
+            if (this.elements[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int removeElementAtIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            // should throw IndexOutOfBoundsException
+            System.out.println("Throwing IndexOutOfBoundsException when trying to" +
+                    " remove element at position " + index);
+            return -1;
+        }
+
+        int removedElement = this.elements[index];
+        for (int i=index; i<this.size; i++) {
+            this.elements[i] = this.elements[i+1];
+        }
+        this.size--;
+
+        return removedElement;
+    }
+
+    public void clear() {
+        this.size = 0;
+    }
+
+    public int set(int index, int e) {
+        if (index < 0 || index >= this.size) {
+            // should throw IndexOutOfBoundsException
+            System.out.println("Throwing IndexOutOfBoundsException when trying to set" +
+                    " the element " + e + " at position " + index);
+            return -1;
+        }
+
+        int replacedElement = this.elements[index];
+        this.elements[index] = e;
+        return replacedElement;
+    }
+
+}
