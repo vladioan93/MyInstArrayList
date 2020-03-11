32e0c10340eed67b27ada33b1792ff22ecd76e00
vlad <prioteasa.vlad@gmail.com>
2020-03-11 15:37:53 +0200

    terminare clasa MyIntArrayList + exemple in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
 public class Main {
     public static void main(String[] args) {
         MyIntArrayList arr = new MyIntArrayList();
-        System.out.println(arr.toString());
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.printArray();
+
+        System.out.println("\n" + arr.indexOf(2) + "\n");
+
+
+        MyIntArrayList arr2 = new MyIntArrayList();
+        arr2.add(5);
+        arr2.add(6);
+        arr2.add(7);
+
+        System.out.println();
+        arr2.printArray();
+
+        arr.addAll(2,arr2);
+
+        System.out.println();
+        arr.printArray();
+
+        int[] newArr = arr.toArray();
+
+        System.out.println();
+
+        for(int i=0;i<newArr.length;i++){
+            System.out.print(newArr[i] + " ");
+        }
+
+        System.out.println("\ncapacitate minima 10 elemente");
+        arr.ensureCapacity(10);
+        arr.printArray();
+
+        System.out.println("\nDupa trim: ");
+        arr.trimToSize();
+        arr.printArray();
     }
 }
--- src/MyIntArrayList.java
+++ src/MyIntArrayList.java
@@ -46,6 +46,23 @@ public class MyIntArrayList {
         return this.size;
     }
 
+    public boolean contains(int e) {
+        for (int i = 0; i < this.size; i++) {
+            if (elements[i] == e) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int get(int index) {
+        if (index > size - 1 || index < 0) {
+            return -1;
+        }
+        return elements[index];
+    }
+
     public int indexOf(int e) {
         for (int i = 0; i < this.size; i++) {
             if (this.elements[i] == e) {
@@ -53,6 +70,30 @@ public class MyIntArrayList {
             }
         }
         return -1;
+    }
+
+    public int lastIndexOf(int e) {
+        for (int i = this.size - 1; i >= 0; i--) {
+            if (this.elements[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean remove(int e) {
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i] == e) {
+                System.out.println("Removed element " + e + " from index " + i);
+                for (int j = i; j < size - 1; j++) {
+                    elements[j] = elements[j + 1];
+                }
+                this.size -= 1;
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public int removeElementAtIndex(int index) {
@@ -89,9 +130,59 @@ public class MyIntArrayList {
         return replacedElement;
     }
 
-    public void printArray(){
-        for(int i=0;i<size;i++){
-            System.out.println();
+    public boolean addAll(MyIntArrayList c) {
+        int initialSize = this.size;
+        this.size += c.size;
+
+        for (int i = 0; i < c.size; i++) {
+            this.elements[initialSize + i] = c.elements[i];
+        }
+
+        return true;
+    }
+
+
+    public boolean addAll(int index, MyIntArrayList c) {
+        if (index > this.size - 1 || index < 0) {
+            return false;
+        }
+        int initialSize = this.size;
+        this.size += c.size;
+
+        for (int i = 0; i < initialSize - index; i++) {
+            this.elements[initialSize + i + 1] = this.elements[index + i];
+        }
+
+        for (int i = 0; i < c.size; i++) {
+            this.elements[index + i] = c.elements[i];
+        }
+
+        return true;
+    }
+
+    public int[] toArray() {
+        int[] arr = new int[this.size];
+
+        for (int i = 0; i < this.size; i++) {
+            arr[i] = this.elements[i];
+        }
+
+        return arr;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        this.size = minCapacity;
+    }
+
+    public void trimToSize() {
+        while (lastIndexOf(0) == this.size - 1) {
+            this.size--;
+        }
+    }
+
+    public void printArray() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(this.elements[i] + ", ");
         }
     }
 
